fd29aa00492f5d8b42ba36307ba85432965a3b98
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2021-05-03 18:31:17 +0200

    package/gr-drm: fix build

--- package/gr-drm/gr-drm.mk
+++ package/gr-drm/gr-drm.mk
@@ -8,7 +8,7 @@ GR_DRM_SITE = $(call github,kit-cel,gr-drm,$(GR_DRM_VERSION))
 GR_DRM_INSTALL_STAGING = NO
 GR_DRM_LICENSE = GPL-3.0
 GR_DRM_LICENSE_FILES = LICENSE
-GR_DRM_DEPENDENCIES = faac
+GR_DRM_DEPENDENCIES = boost faac cppunit
 
 $(eval $(cmake-package))
 
