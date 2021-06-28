22458b80b7dda27fd25a2ed2451916f3b8bcd044
MathisMARION <mathis.marion1@gmail.com>
2021-06-28 16:12:17 +0200

    fmc-adc-100m package

---
+++ package/fmc-adc-100m/fmc-adc-100m.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# fmc-adc-100m
+#
+################################################################################
+
+FMC_ADC_100M_VERSION = 0e644a55b276d1a82277be09e711e09a544b8414
+FMC_ADC_100M_SITE = https://ohwr.org/project/fmc-adc-100m14b4cha-sw
+FMC_ADC_100M_SITE_METHOD = git
+
+FMC_ADC_100M_INSTALL_STAGING = YES
+
+FMC_ADC_100M_DEPENDENCIES += zio
+
+define FMC_ADC_100M_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) \
+		-C $(@D)/tools
+endef
+
+define FMC_ADC_100M_INSTALL_STAGING_CMDS
+	$(INSTALL) -m 0644 $(@D)/kernel/fmc-adc-100m14b4cha.h $(STAGING_DIR)/include
+endef
+
+define FMC_ADC_100M_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) \
+		-C $(@D)/tools install \
+		DESTDIR=$(TARGET_DIR)
+endef
+
+$(eval $(generic-package))
