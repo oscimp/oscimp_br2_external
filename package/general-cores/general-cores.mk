471e1bb55da61e8ab18308b6cd0d8f022881c0cf
MathisMARION <mathis.marion1@gmail.com>
2021-08-20 11:41:30 +0200

    General cores kernel modules

---
+++ package/general-cores/general-cores.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# general-cores
+#
+################################################################################
+
+GENERAL_CORES_VERSION = v1.1.0
+GENERAL_CORES_SITE = https://ohwr.org/project/general-cores
+GENERAL_CORES_SITE_METHOD = git
+
+GENERAL_CORES_MODULE_SUBDIRS = software/htvic/drivers
+GENERAL_CORES_MODULE_SUBDIRS += software/i2c-ocores/drivers/i2c/busses
+GENERAL_CORES_MODULE_SUBDIRS += software/spi-ocores/drivers/spi
+GENERAL_CORES_MODULE_MAKE_OPTS = CONFIG_I2C_OCORES=m
+GENERAL_CORES_MODULE_MAKE_OPTS += CONFIG_SPI_OCORES=m
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
