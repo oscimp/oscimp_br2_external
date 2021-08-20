50502ba72db64780fbd12875c600420b7239a727
MathisMARION <mathis.marion1@gmail.com>
2021-08-20 15:07:15 +0200

    Installed FPGA manager headers into staging

--- package/fpga-manager/fpga-manager.mk
+++ package/fpga-manager/fpga-manager.mk
@@ -8,8 +8,15 @@ FPGA_MANAGER_VERSION = 4bfe26e2a451cb104c5a13ac102e52d6240fa432
 FPGA_MANAGER_SITE = https://gitlab.cern.ch/coht/fpga-manager
 FPGA_MANAGER_SITE_METHOD = git
 
+FPGA_MANAGER_INSTALL_STAGING = YES
+
 FPGA_MANAGER_MODULE_SUBDIRS = drivers/fpga
 FPGA_MANAGER_MODULE_MAKE_OPTS = CONFIG_FPGA=m
+
+define FPGA_MANAGER_INSTALL_STAGING_CMDS
+	$(INSTALL) -d $(STAGING_DIR)/usr/include/linux/fpga
+	$(INSTALL) -m 0644 $(@D)/include/linux/fpga/*.h $(STAGING_DIR)/usr/include/linux/fpga
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
