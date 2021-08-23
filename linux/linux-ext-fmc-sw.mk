c4a6f315de13162ec8494a0044f78a7cced40c2f
MathisMARION <mathis.marion1@gmail.com>
2021-08-23 10:30:21 +0200

    FMC-sw linux extension

--- linux/linux-ext-fmc-sw.mk
+++ linux/linux-ext-fmc-sw.mk
@@ -7,22 +7,31 @@
 LINUX_EXTENSIONS += fmc-sw
 
 define FMC_SW_PREPARE_KERNEL
-	DEST_DIR=$(LINUX_DIR)/drivers/wrtd_ref_spec150t_adc/fmc_sw; \
+	`# Create destination directory`; \
+	DEST_DIR=$(LINUX_DIR)/drivers/wrtd_ref_spec150t_adc; \
+	mkdir -p $${DEST_DIR}; \
+	if [ ! -e $${DEST_DIR}/Kconfig ]; then \
+		echo "source \"drivers/wrtd_ref_spec150t_adc/Kconfig\"" >> $(LINUX_DIR)/drivers/Kconfig; \
+	fi; \
 	\
 	`# Copy headers`; \
 	cp -dpfr $(FMC_SW_DIR)/include/* $(LINUX_DIR)/include; \
 	\
 	`# Copy sources`; \
-	mkdir -p $${DEST_DIR}; \
-	cp -dpfr $(FMC_SW_DIR)/drivers/fmc/* $${DEST_DIR}; \
+	cp -dpfr $(FMC_SW_DIR)/drivers/fmc $${DEST_DIR}; \
 	\
 	`# Add a Kconfig`; \
-	echo "config FMC_SW" >> $${DEST_DIR}/Kconfig; \
-	echo -e "\ttristate \"FMC software driver for Linux\"" >> $${DEST_DIR}/Kconfig; \
-	echo -e "\tdefault m" >> $${DEST_DIR}/Kconfig; \
-	echo -e "\thelp" >> $${DEST_DIR}/Kconfig; \
-	echo -e "\t  This project aims to provide basic support to FMC modules with respect" >> $${DEST_DIR}/Kconfig; \
-	echo -e "\t  to the VITA 57.1 standard." >> $${DEST_DIR}/Kconfig; \
+	echo "config FMC" >> $${DEST_DIR}/fmc/Kconfig; \
+	echo -e "\ttristate \"FMC software driver for Linux\"" >> $${DEST_DIR}/fmc/Kconfig; \
+	echo -e "\tdefault m" >> $${DEST_DIR}/fmc/Kconfig; \
+	echo -e "\thelp" >> $${DEST_DIR}/fmc/Kconfig; \
+	echo -e "\t  This project aims to provide basic support to FMC modules with respect" >> $${DEST_DIR}/fmc/Kconfig; \
+	echo -e "\t  to the VITA 57.1 standard." >> $${DEST_DIR}/fmc/Kconfig; \
 	echo -e "\t  " >> $${DEST_DIR}/Kconfig; \
-	echo -e "\t  https://ohwr.org/project/fmc-sw" >> $${DEST_DIR}/Kconfig
+	echo -e "\t  https://ohwr.org/project/fmc-sw" >> $${DEST_DIR}/fmc/Kconfig; \
+	\
+	echo "source \"$${DEST_DIR}/fmc/Kconfig\"" >> $${DEST_DIR}/Kconfig; \
+	\
+	`# Edit Makefile`; \
+	echo "obj-m += fmc/" >> $${DEST_DIR}/Makefile;
 endef
