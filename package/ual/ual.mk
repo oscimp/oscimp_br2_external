50f459fb31b5c0583048a6440cc58af1b65b43b2
MathisMARION <mathis.marion1@gmail.com>
2021-08-25 12:19:23 +0200

    UAL package

---
+++ package/ual/ual.mk
@@ -0,0 +1,37 @@
+################################################################################
+#
+# ual
+#
+################################################################################
+
+UAL_VERSION = 42a5d4923d47cf65e171d0257bf30efe7c660be9
+UAL_SITE = https://gitlab.cern.ch/cohtdrivers/ual
+UAL_SITE_METHOD = git
+
+UAL_INSTALL_STAGING = YES
+
+UAL_PYTHON_DIR = python$(PYTHON3_VERSION_MAJOR)
+
+define UAL_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) \
+		-C $(@D)/lib
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) \
+		-C $(@D)/tools
+endef
+
+define UAL_INSTALL_STAGING_CMDS
+	$(INSTALL) -m 0755 $(@D)/lib/libual.a $(STAGING_DIR)/usr/lib
+	$(INSTALL) -m 0644 $(@D)/lib/ual.h $(STAGING_DIR)/usr/include
+endef
+
+define UAL_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/lib/libual.a $(TARGET_DIR)/usr/lib
+	$(INSTALL) -m 0755 $(@D)/tools/ualmem $(TARGET_DIR)/usr/bin
+	( \
+		cd $(@D)/PyUAL; \
+		$(HOST_DIR)/bin/python3 setup.py install \
+			--prefix=$(TARGET_DIR)/usr \
+	)
+endef
+
+$(eval $(generic-package))
