0286bbf2a9d436d14d0c9a2d860c0d069bd90ce8
MathisMARION <mathis.marion1@gmail.com>
2021-08-27 11:43:04 +0200

    FPGA Manager kernel module

--- package/fpga-manager/fpga-manager.mk
+++ package/fpga-manager/fpga-manager.mk
@@ -8,4 +8,8 @@ FPGA_MANAGER_VERSION = 4bfe26e2a451cb104c5a13ac102e52d6240fa432
 FPGA_MANAGER_SITE = https://gitlab.cern.ch/coht/fpga-manager
 FPGA_MANAGER_SITE_METHOD = git
 
+FPGA_MANAGER_MODULE_SUBDIRS = drivers/fpga
+FPGA_MANAGER_MODULE_MAKE_OPTS = CONFIG_FPGA=m
+
+$(eval $(kernel-module))
 $(eval $(generic-package))
