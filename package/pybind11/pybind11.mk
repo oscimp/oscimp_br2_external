486673b4b4cb82b6631a3c78f5704ae3d9db5704
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2021-12-05 12:15:48 +0100

    unlock staging + target possibility

--- package/pybind11/pybind11.mk
+++ package/pybind11/pybind11.mk
@@ -11,31 +11,49 @@ PYBIND11_LICENSE_FILES = LICENSE
 PYBIND11_INSTALL_STAGING = YES
 PYBIND11_SUPPORTS_IN_SOURCE_BUILD = YES
 
+PYBIND11_CONF_OPTS = \
+	-DBUILD_DOCS=OFF \
+	-DPYBIND11_TEST=OFF \
+	-DPYBIND11_NOPYTHON=ON
+
 HOST_PYBIND11_CONF_OPTS = \
-	-DBUILD_DOCS=OFF \
-	-DDOWNLOAD_EIGEN=OFF
+	-DBUILD_DOCS=OFF \
+	-DPYBIND11_TEST=OFF \
+	-DPYBIND11_NOPYTHON=ON
+
+## package uses DESTDIR
+#define PYBIND11_INSTALL_CMDS
+#	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+#		prefix=/usr DESTDIR=$(TARGET_DIR) install
+#endef
+
+#PYBIND11_INSTALL_STAGING_OPTS = DESTDIR=/usr PREFIX=$(STAGING_DIR)
+#PYBIND11_INSTALL_TARGET_OPTS = PREFIX=$(TARGET_DIR)
 
 # pybind11 python support activation
 ifeq ($(BR2_PACKAGE_PYBIND11_WITH_PYTHON),y)
+PYBIND11_DEPENDENCIES += host-python3
 HOST_PYBIND11_DEPENDENCIES += host-python3
 
-# pybind11 with python requires cmake install in $(@D)
-HOST_PYBIND11_CONF_OPTS += \
-	-DCMAKE_INSTALL_PREFIX=$(@D)/pybind11 \
-	-DPYTHON=$(HOST_DIR)/bin/python3 \
-	-DPYTHON_PREFIX=$(STAGING_DIR)/usr \
-	-DPYBIND_FINDPYTHON=ON \
-	-DPYBIND11_NOPYTHON=OFF
+# building python side of this package
+# requires cmake install in $(@D)/pybind11
+PYBIND11_CONF_OPTS += \
+	-DPYTHON=$(HOST_DIR)/usr/bin/python3 \
+	-DPYTHON_PREFIX=$(STAGING_DIR)/usr
+	#-DCMAKE_INSTALL_PREFIX=$(@D)/pybind11 \
 
+HOST_PYBIND11_CONF_OPTS += \
+	-DPYTHON=$(HOST_DIR)/usr/bin/python3 \
+	-DPYTHON_PREFIX=$(HOST_DIR)/usr
+	#-DCMAKE_INSTALL_PREFIX=$(@D)/pybind11 \
+
+# trick to build python side of this package
 define PYBIND11_PYTHON_BUILD
-	cd $(@D) && $(HOST_DIR)/bin/python setup.py install
+	cd $(@D) && $(HOST_DIR)/usr/bin/python3 setup.py install
 endef
 
 HOST_PYBIND11_POST_INSTALL_HOOKS += PYBIND11_PYTHON_BUILD
-else
-HOST_PYBIND11_CONF_OPTS += \
-	-DPYBIND_FINDPYTHON=OFF \
-	-DPYBIND11_NOPYTHON=ON
 endif
 
+$(eval $(cmake-package))
 $(eval $(host-cmake-package))
