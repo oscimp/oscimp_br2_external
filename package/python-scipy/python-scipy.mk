aa68caa5b84982126bc5ab679ddea7654763ba51
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-02-20 12:53:09 +0100

    python-scipy update to current patch serie

--- package/python-scipy/python-scipy.mk
+++ package/python-scipy/python-scipy.mk
@@ -4,51 +4,69 @@
 #
 ################################################################################
 
-PYTHON_SCIPY_VERSION = 1.7.2
-PYTHON_SCIPY_SOURCE = v$(PYTHON_SCIPY_VERSION).tar.gz
-PYTHON_SCIPY_SITE = https://github.com/scipy/scipy/archive
-PYTHON_SCIPY_LICENSE = BSD-3-Clause
-PYTHON_SCIPY_LICENSE_FILES = LICENSE.txt doc/sphinxext/LICENSE.txt \
-			doc/scipy-sphinx-theme/LICENSE.txt
-PYTHON_SCIPY_SETUP_TYPE = setuptools
-
-PYTHON_SCIPY_DEPENDENCIES = host-python-numpy \
-	boost \
-	host-python-pybind \
+PYTHON_SCIPY_VERSION = 1.7.3
+PYTHON_SCIPY_SOURCE = scipy-$(PYTHON_SCIPY_VERSION).tar.xz
+PYTHON_SCIPY_SITE = https://github.com/scipy/scipy/releases/download/v$(PYTHON_SCIPY_VERSION)
+PYTHON_SCIPY_LICENSE = \
+	BSD-3-Clause, \
+	BSD-2-Clause, \
+	BSD, \
+	BSD-Style, \
+	Apache-2.0, \
+	MIT
+PYTHON_SCIPY_LICENSE_FILES = \
+	LICENSE.txt \
+	scipy/linalg/src/lapack_deprecations/LICENSE \
+	scipy/ndimage/LICENSE.txt \
+	scipy/optimize/tnc/LICENSE \
+	scipy/sparse/linalg/dsolve/SuperLU/License.txt \
+	scipy/sparse/linalg/eigen/arpack/ARPACK/COPYING \
+	scipy/spatial/qhull_src/COPYING.txt
+PYTHON_SCIPY_DEPENDENCIES += \
+	host-meson \
+	host-python-numpy \
 	host-python-pythran \
-	host-python-gast \
-	host-python-beniget \
-	host-python-ply
+	lapack \
+	openblas \
+	python-numpy \
+	python-pybind
+PYTHON_SCIPY_INSTALL_STAGING = YES
 
-# blas support may be provided by lapack (libblas.a) or openblas (libopenblas.a)
-ifeq ($(BR2_PACKAGE_PYTHON_SCIPY_OPENBLAS),y)
-PYTHON_SCIPY_DEPENDENCIES += openblas
-else
-PYTHON_SCIPY_DEPENDENCIES += lapack
-endif
+HOST_PYTHON_SCIPY_DEPENDENCIES += \
+	host-meson \
+	host-python-numpy \
+	host-python-pythran \
+	host-lapack \
+	host-openblas \
+	python-numpy \
+	host-python-pybind
 
-PYTHON_SCIPY_CFLAGS = "$(TARGET_CFLAGS)"
-PYTHON_SCIPY_LDFLAGS = "$(TARGET_LDFLAGS) -shared -L$(PYTHON3_PATH)/site-packages/numpy/core/lib"
-PYTHON_SCIPY_ENV += CFLAGS=$(PYTHON_SCIPY_CFLAGS)
-PYTHON_SCIPY_ENV += LDFLAGS=$(PYTHON_SCIPY_LDFLAGS)
+PYTHON_SCIPY_SETUP_TYPE = setuptools
+PYTHON_SCIPY_BUILD_OPTS = config_fc --fcompiler=gnu95
 
-HOST_PYTHON_SCIPY_CFLAGS = "$(HOST_CFLAGS)"
-HOST_PYTHON_SCIPY_LDFLAGS = "$(HOST_LDFLAGS) -shared -L$(PYTHON3_PATH)/site-packages/numpy/core/lib"
-HOST_PYTHON_SCIPY_ENV += CFLAGS=$(HOST_PYTHON
+# -lnpymath npymath.ini localization
+PYTHON_SCIPY_NPY_PKG_CONFIG_PATH = \
+	$(STAGING_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/numpy/core/lib/npy-pkg-config
 
-# must be used to locate fortran compiler
-PYTHON_SCIPY_ENV += F90="$(TARGET_FC)"
+PYTHON_SCIPY_LDFLAGS = $(TARGET_LDFLAGS) -shared \
+	-L$(PYTHON3_PATH)/site-packages/numpy/core/lib
+# -lnpyrandom localization
+PYTHON_SCIPY_LDFLAGS += \
+	-L$(STAGING_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/numpy/random/lib
 
-# tick to locate 'lapack' and 'blas'
-define PYTHON_SCIPY_LOCATE_LAPACK_BLAS_LIBS
-	rm -f $(@D)/site.cfg
+PYTHON_SCIPY_ENV = \
+	F90=$(TARGET_FC) \
+	NPY_PKG_CONFIG_PATH=$(PYTHON_SCIPY_NPY_PKG_CONFIG_PATH) \
+	LDFLAGS="$(PYTHON_SCIPY_LDFLAGS)"
+
+# Provide system configuration options to numpy distutils extenions, telling
+# to find all include files and libraries in staging directory.
+define PYTHON_SCIPY_CONFIGURE_CMDS
+	-rm -f $(@D)/site.cfg
 	echo "[DEFAULT]" >> $(@D)/site.cfg
 	echo "library_dirs = $(STAGING_DIR)/usr/lib" >> $(@D)/site.cfg
 	echo "include_dirs = $(STAGING_DIR)/usr/include" >> $(@D)/site.cfg
 endef
-
-PYTHON_SCIPY_PRE_CONFIGURE_HOOKS += PYTHON_SCIPY_LOCATE_LAPACK_BLAS_LIBS
-HOST_PYTHON_SCIPY_PRE_CONFIGURE_HOOKS += PYTHON_SCIPY_LOCATE_LAPACK_BLAS_LIBS
 
 $(eval $(python-package))
 $(eval $(host-python-package))
