7de0df997c64fcd6752c95651f8ea02dcff1ea84
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-02-20 13:08:43 +0100

    python-scikitlearn: upgrade to latest version, improved .mk

--- package/python-scikitlearn/python-scikitlearn.mk
+++ package/python-scikitlearn/python-scikitlearn.mk
@@ -4,29 +4,18 @@
 #
 ################################################################################
 
-PYTHON_SCIKITLEARN_VERSION = 0.23.1
+PYTHON_SCIKITLEARN_VERSION = 1.0.2
 PYTHON_SCIKITLEARN_SITE = $(call github,scikit-learn,scikit-learn,$(PYTHON_SCIKITLEARN_VERSION))
 PYTHON_SCIKITLEARN_LICENSE = BSD-3-Clause
 PYTHON_SCIKITLEARN_LICENSE_FILES = COPYING
 PYTHON_SCIKITLEARN_SETUP_TYPE = setuptools
-PYTHON_SCIKITLEARN_DEPENDENCIES = host-python-cython \
-	host-python-joblib \
+PYTHON_SCIKITLEARN_DEPENDENCIES = \
+	host-python-cython \
 	host-python-scipy \
-	openblas \
-	lapack
+	host-python-joblib \
+	host-python-threadpoolctl
 
-#https://github.com/scikit-learn/scikit-learn/archive/0.23.1.tar.gz
-# must be used to locate 'gfortran'
-#PYTHON_SCIKITLEARN_ENV += F90="$(TARGET_FC)"
-
-# trick to locate 'lapack' and 'blas'
-define PYTHON_SCIKITLEARN_FIND_LAPACK_BLAS
-	rm -f $(@D)/site.cfg
-	echo "[DEFAULT]" >> $(@D)/site.cfg
-	echo "library_dirs = $(STAGING_DIR)/usr/lib" >> $(@D)/site.cfg
-	echo "include_dirs = $(STAGING_DIR)/usr/include" >> $(@D)/site.cfg
-endef
-
-PYTHON_SCIKITLEARN_PRECONFIGURE_HOOKS += PYTHON_SCIKITLEARN_FIND_LAPACK_BLAS
+# disable ./test_program when cross compiling
+PYTHON_SCIKITLEARN_ENV = PYTHON_CROSSENV=1
 
 $(eval $(python-package))
