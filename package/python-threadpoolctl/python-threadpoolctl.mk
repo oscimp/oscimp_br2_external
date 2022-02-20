99312d48b01ede5a755148abcf54c0ec23e1eb15
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-02-20 12:53:36 +0100

    python-threadpoolctl: host-only package

--- package/python-threadpoolctl/python-threadpoolctl.mk
+++ package/python-threadpoolctl/python-threadpoolctl.mk
@@ -4,11 +4,19 @@
 #
 ################################################################################
 
-PYTHON_THREADPOOLCTL_VERSION = 2.1.0
-PYTHON_THREADPOOLCTL_SITE = $(call github,joblib,threadpoolctl,$(PYTHON_THREADPOOLCTL_VERSION))
+PYTHON_THREADPOOLCTL_VERSION = 3.1.0
+PYTHON_THREADPOOLCTL_SITE = $(call github,joblib,threadpoolctl,$(PYTHON_THREADPOOLCTL_VERSION),$(PYTHON_THREADPOOLCTL_VERSION).tar.gz)
 PYTHON_THREADPOOLCTL_LICENSE = BSD-3-Clause
 PYTHON_THREADPOOLCTL_LICENSE_FILES = LICENSE
-PYTHON_THREADPOOLCTL_SETUP_TYPE = distutils
 
-$(eval $(python-package))
-$(eval $(host-python-package))
+# threadpoolctl is originally built using 'flit' which
+# simply symlinks $(@)/threadpoolctl.py to site-packages
+define PYTHON_THREADPOOLCTL_BUILD_CMDS
+endef
+
+define PYTHON_THREADPOOLCTL_INSTALL_CMDS
+	ln -sf $(@)/threadpoolctl.py \
+		$(HOST_DIR)/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/threadpoolctl.py
+endef
+
+$(eval $(host-generic-package))
