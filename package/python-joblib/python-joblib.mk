b23a1e2ccce8a39d4b042f43a704d1d4539be567
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2022-02-22 19:56:53 +0100

    sklearn needs python-joblib as is too

--- package/python-joblib/python-joblib.mk
+++ package/python-joblib/python-joblib.mk
@@ -10,4 +10,10 @@ PYTHON_JOBLIB_LICENSE = BSD-3-Clause
 PYTHON_JOBLIB_LICENSE_FILES = LICENSE.txt
 PYTHON_JOBLIB_SETUP_TYPE = setuptools
 
+define REMOVE_TESTS_FOR_CHINESE_SPECIAL_CHARACTERS
+	rm -f $(TARGET_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/joblib/test/test_func_inspect_special_encoding.py
+endef
+PYTHON_JOBLIB_POST_INSTALL_TARGET_HOOKS += REMOVE_TESTS_FOR_CHINESE_SPECIAL_CHARACTERS
+
+$(eval $(python-package))
 $(eval $(host-python-package))
