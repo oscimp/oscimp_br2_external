dfe4ee8b2a84377befc2c29693e0ca0721d0db75
JM Friedt <jmfriedt@femto-st.fr>
2025-02-09 08:46:45 +0000

    remove gr-iio which is not part of GNU Radio + update XTRX version to compile with latest kernel

--- package/xtrx_linux_pcie_drv/xtrx_linux_pcie_drv.mk
+++ package/xtrx_linux_pcie_drv/xtrx_linux_pcie_drv.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XTRX_LINUX_PCIE_DRV_VERSION = 5ae3a3e877e5b04da6e1cfdcca2d2f124857ddb0
+XTRX_LINUX_PCIE_DRV_VERSION = d218d3e8be3f723000bdfff6b6235a85f7b10e42
 XTRX_LINUX_PCIE_DRV_SITE = $(call github,xtrx-sdr,xtrx_linux_pcie_drv,$(XTRX_LINUX_PCIE_DRV_VERSION))
 XTRX_LINUX_PCIE_DRV_LICENSE = GPL-2.0
 XTRX_LINUX_PCIE_DRV_LICENSE_FILES = xtrx.c
