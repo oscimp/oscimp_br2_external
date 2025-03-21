16ee106d607d391da885f267664491afee8b8075
jmfriedt <jmfriedt@femto-st.fr>
2025-03-21 10:44:43 +0100

    remove contradictory flags to write to target

--- package/libfec/libfec.mk
+++ package/libfec/libfec.mk
@@ -10,20 +10,18 @@ LIBFEC_INSTALL_STAGING = YES
 LIBFEC_INSTALL_TARGET = YES
 LIBFEC_LICENSE = BSD-2
 LIBFEC_LICENSE_FILES = LICENSE.txt
-LIBFEC_INSTALL_TARGET = NO
 
 define LIBFEC_BUILD_CMDS
     $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/
 endef
 
 define LIBFEC_INSTALL_STAGING_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/libfec.so $(STAGING_DIR)/usr/lib/
-    $(INSTALL) -D -m 0755 $(@D)/libfec.a $(STAGING_DIR)/usr/lib/
+    $(INSTALL) -D -m 0644 $(@D)/libfec.so $(STAGING_DIR)/usr/lib/
+    $(INSTALL) -D -m 0644 $(@D)/libfec.a $(STAGING_DIR)/usr/lib/
 endef
     
 define LIBFEC_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/libfec.so $(TARGET_DIR)/usr/lib
-    $(INSTALL) -D -m 0755 $(@D)/libfec.a $(TARGET_DIR)/usr/lib
+    $(INSTALL) -D -m 0644 $(@D)/libfec.so $(TARGET_DIR)/usr/lib
 endef
 
 $(eval $(autotools-package))
--- package/pocketsdr/pocketsdr.mk
+++ package/pocketsdr/pocketsdr.mk
@@ -8,22 +8,19 @@ POCKETSDR_VERSION = 82e339325f3af5e8f6852ad4b80d37effc7eec72
 POCKETSDR_SITE = $(call github,tomojitakasu,PocketSDR,$(POCKETSDR_VERSION))
 
 # POCKETSDR_GIT_SUBMODULES = YES
-POCKETSDR_INSTALL_STAGING = YES
+POCKETSDR_INSTALL_TARGET = YES
 POCKETSDR_LICENSE = BSD-2
 POCKETSDR_LICENSE_FILES = LICENSE.txt
 POCKETSDR_DEPENDENCIES = libfec ldpc libusb fftw-single
 
 define POCKETSDR_BUILD_CMDS
     $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/lib/build -f librtk.mk
-    $(INSTALL) -D -m 0755 $(@D)/lib/build/librtk.a  $(STAGING_DIR)/usr/lib/
-    $(INSTALL) -D -m 0755 $(@D)/lib/build/librtk.so $(STAGING_DIR)/usr/lib/
+    $(INSTALL) -D -m 0644 $(@D)/lib/build/librtk.a  $(STAGING_DIR)/usr/lib/
+    $(INSTALL) -D -m 0644 $(@D)/lib/build/librtk.so $(STAGING_DIR)/usr/lib/
     $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/lib/build -f libsdr.mk
-    $(INSTALL) -D -m 0755 $(@D)/lib/build/libsdr.a  $(STAGING_DIR)/usr/lib/
-    $(INSTALL) -D -m 0755 $(@D)/lib/build/libsdr.so  $(STAGING_DIR)/usr/lib/
+    $(INSTALL) -D -m 0644 $(@D)/lib/build/libsdr.a  $(STAGING_DIR)/usr/lib/
+    $(INSTALL) -D -m 0644 $(@D)/lib/build/libsdr.so  $(STAGING_DIR)/usr/lib/
     $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/app
-endef
-
-define POCKETSDR_INSTALL_STAGING_CMDS
 endef
 
 define POCKETSDR_INSTALL_TARGET_CMDS
@@ -34,10 +31,8 @@ define POCKETSDR_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/app/pocket_trk/pocket_trk $(TARGET_DIR)/usr/bin
 	$(INSTALL) -D -m 0755 $(@D)/app/pocket_scan/pocket_scan $(TARGET_DIR)/usr/bin
 	$(INSTALL) -D -m 0755 $(@D)/app/pocket_snap/pocket_snap $(TARGET_DIR)/usr/bin
-	$(INSTALL) -D -m 0755 $(@D)/lib/build/librtk.a $(TARGET_DIR)/usr/lib
-	$(INSTALL) -D -m 0755 $(@D)/lib/build/librtk.so $(TARGET_DIR)/usr/lib
-	$(INSTALL) -D -m 0755 $(@D)/lib/build/libsdr.a $(TARGET_DIR)/usr/lib
-	$(INSTALL) -D -m 0755 $(@D)/lib/build/libsdr.so $(TARGET_DIR)/usr/lib
+	$(INSTALL) -D -m 0644 $(@D)/lib/build/librtk.so $(TARGET_DIR)/usr/lib
+	$(INSTALL) -D -m 0644 $(@D)/lib/build/libsdr.so $(TARGET_DIR)/usr/lib
 endef
 
 $(eval $(generic-package))
